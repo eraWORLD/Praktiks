2f5fea3d0f3ca0fa6cb8db4cec8fef5b751086bf
eraWORLD <68866670+eraWORLD@users.noreply.github.com>
2020-11-15 14:06:03 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {NgModule, Provider} from '@angular/core';
+import {registerLocaleData} from "@angular/common";
+import ruLocale from "@angular/common/locales/ru";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PostPageComponent } from './post-page/post-page.component';
+import { PostComponent } from './shared/components/post/post.component';
+import {SheredModule} from './shared/shered.module';
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {AuthInterceptor} from "./shared/auth.interceptor";
+import {register} from "ts-node";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+
+registerLocaleData(ruLocale, 'ru');
+
+const INTERCEPTOR_PROVIDER: Provider = {
+  provide: HTTP_INTERCEPTORS,
+  multi: true,
+  useClass: AuthInterceptor,
+};
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainLayoutComponent,
+    HomePageComponent,
+    PostPageComponent,
+    PostComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+      SheredModule,
+      ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+  ],
+  providers: [INTERCEPTOR_PROVIDER],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiKey: 'AIzaSyDAMLQuMAGZ_bGaDKe6CmgejO2xmF194iU',
+  fbDbUrl: 'https://angular-blog-f56ba.firebaseio.com/',
+};
